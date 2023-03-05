963eb92ed71f0778dc93585ffafcba9698c935e3
OliwiaWawrzynek <oliwiawawrzynek@gmail.com>
2023-03-05 22:43:24 +0100

    goit-react-hw-01-components

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import friend from './friends.json';
+import clsx from "clsx";
+
+export const FriendList = () => {
+    return (
+        <ul className="friend-list">
+            <FriendListItem />
+        </ul>
+    );
+};
+
+export const FriendListItem = () => {
+    return (
+        <ul className="friend-list">
+            {friend.map(friend => (
+                <li 
+                    className={clsx(
+                        "item",
+                        friend.isOnline && "status"
+                    )}
+                    key={friend.id}>
+                    <span className= "status">{friend.isOnline}</span>
+                    <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
+                    <p className="name">{friend.name}</p>
+                </li>
+            ))}
+        </ul>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList'
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory'
+
+import PropTypes from "prop-types";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Profile />
+    <Statistics />
+    <FriendList />
+    <TransactionHistory />
   </React.StrictMode>
 );
+
+Profile.PropTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.number.isRequired,
+};
+
+Statistics.PropTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.array.isRequired,
+};
+
+FriendList.PropTypes = {
+  friends: PropTypes.array.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+TransactionHistory.PropTypes = {
+  items: PropTypes.array.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+};
